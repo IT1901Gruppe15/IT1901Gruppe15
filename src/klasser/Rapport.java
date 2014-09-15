9ac3c3d08b435194dbf742511fd36deb6807e7f7
morten2311 <morten23_lundenes@hotmail.com>
2014-09-15 20:48:31 +0200

    Klasser
    Lagt til midlertidig klasser

---
+++ src/klasser/Rapport.java
@@ -0,0 +1,34 @@
+package klasser;
+
+import java.io.BufferedReader;
+import java.io.Reader;
+import java.util.List;
+
+public class Rapport {
+	private int koieID;
+	private List<Utstyr> odelagtUtstyr;
+	private int vedstatus;
+	
+	public void lesRapport(Reader input){
+		BufferedReader reader = new BufferedReader(input);
+		while(reader.ready()){
+			
+		}
+		
+	}
+	public void endreUtstyrStatus(){
+		
+	}
+	public void oppdaterVedStatus(){
+		
+	}
+	public void oppdaterGjenglemt(){
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/klasser/Reservasjon.java
@@ -0,0 +1,10 @@
+package klasser;
+
+public class Reservasjon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
