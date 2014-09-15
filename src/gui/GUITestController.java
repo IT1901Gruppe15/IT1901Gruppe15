c3c27f7937801e562d98add07cc3b8d937798278
krislerv <kristoffer-lervik@hotmail.com>
2014-09-15 23:41:27 +0200

    Revert "liten gui update"
    This reverts commit fd9976b11fddb2fae1689dce5178f672007388eb.

--- src/gui/GUITestController.java
+++ src/gui/GUITestController.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
@@ -13,14 +12,14 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 public class GUITestController {
 
 	@FXML private BorderPane root;
 	@FXML private Pane loginScreen;
 	@FXML private TextField usernameField;
-	@FXML private PasswordField passwordField;
-	@FXML private Pane koieListe;
+	@FXML private VBox koieListe;
 	@FXML private HBox toolbar;
 	@FXML private ImageView image;
 
@@ -33,11 +32,6 @@ public class GUITestController {
 				}
 			}
 		});
-	}
-	
-	@FXML
-	public void test(ActionEvent event) {
-		System.out.println("test " + event);
 	}
 
 	@FXML
