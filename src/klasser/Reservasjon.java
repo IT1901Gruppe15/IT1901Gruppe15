be435324334e4d032751136501c3b1327263baba
morten2311 <morten23_lundenes@hotmail.com>
2014-09-18 10:19:29 +0200

    Reservasjon

--- src/klasser/Reservasjon.java
+++ src/klasser/Reservasjon.java
@@ -1,31 +1,49 @@
 package klasser;
 
 import java.io.BufferedReader;
+
+import filer.*;
+
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Reader;
 
 public class Reservasjon {
 	String koieID;
-	private String epost,dato;
+	private String epost,start_dato,slutt_dato;
 	
 	public void lesReservasjon(){
 		BufferedReader reader=null; 
-		try{
-		File file = new File("testinput.txt");
-		reader=new BufferedReader(new FileReader(file));
-		String line = null;
-			while((line = reader.readLine())!=null){
-				int pos = line.indexOf(";");
-				if(pos>0){
-					koieID=line.substring(0,pos);
-				}
-				int pos2 = line.indexOf(";",pos+1);
-				epost=line.substring(pos,pos2);
-				dato = line.substring(pos2);
-				
+		
+		try{		
+		InputStream inputStream = Reservasjon.class.getResourceAsStream("testinput.txt");
+
+		reader=new BufferedReader(new InputStreamReader(inputStream));
+		String line = null;	
+		line = reader.readLine();
+		String[] list = line.split(";");
+		for (int i=0;i<list.length;i++){
+			switch(i){
+			case 0:
+				koieID = list[0];
+				break;
+			case 1:
+				epost=list[1];
+				break;
+			case 2:
+				start_dato=list[2];
+				break;
+			case 3:
+				slutt_dato=list[3];
+			
 			}
+			
+		}
+
 		}catch (Exception e){
 		System.err.println(e.getStackTrace());
 			
@@ -37,7 +55,7 @@ public class Reservasjon {
 	public static void main(String[] args) {
 		Reservasjon res = new Reservasjon();
 		res.lesReservasjon();
-		System.out.println(res.epost+res.dato+res.koieID);
+		System.out.println(res.koieID + res.epost+res.start_dato + res.slutt_dato);
 		// TODO Auto-generated method stub
 
 	}
