6671cc47c4cce98ce35222e4f572eb56e26065a4
mariuslundbo <marius.lundbo@gmail.com>
2014-09-23 17:14:09 +0200

    utstyrsid

--- src/klasser/TestDB.java
+++ src/klasser/TestDB.java
@@ -7,21 +7,20 @@ import database.DB;
 
 public class TestDB {
 
-//	public static void main(String[] args) {
-//		DBConnection connection = new DBConnection();
-//		ResultSet rs = connection.login("Marius");
-//		try {
-//			while(rs.next()){
-//				//les kolonne1
-//				System.out.println(rs.getString(1));
-//				//les kolonne2
-//				System.out.println(rs.getString(2));
-//			}
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
+	public static void main(String[] args) {
+		DBConnection connection = new DBConnection();
+		ResultSet rs = connection.getUtstyrID("baat","Flaakoia");
+		try {
+			while(rs.next()){
+				//les kolonne1
+				System.out.println(rs.getString(1));
+				//les kolonne2
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 
 	
