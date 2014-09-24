cb2566500b3fbbde75e922847211cd3d560caa92
Joakim <joakigl@stud.ntnu.no>
2014-09-24 18:44:53 +0200

    bortimot ferdig med testfil

--- src/klasser/Rapport.java
+++ src/klasser/Rapport.java
@@ -3,6 +3,7 @@ package klasser;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,15 +21,14 @@ public class Rapport {
 	public void lesRapport(Reader input) throws IOException {
 		try{
 			BufferedReader reader = new BufferedReader(input);
-			int index = 0;
 			String line = null;
 			String[] ord;
 			String[] odelagt;
 			String[] gjenglemt;
-			odelagtUtstyr = new ArrayList<String>();
-			gjenglemteTing = new ArrayList<String>();
 			
 			while((line = reader.readLine())!=null){
+				odelagtUtstyr = new ArrayList<String>();
+				gjenglemteTing = new ArrayList<String>();
 				ord = line.split("¤");
 				for(int i=0;i<ord.length;i++){
 					switch(i){
@@ -54,15 +54,19 @@ public class Rapport {
 				}
 				connection.settinnRapport("tekst?","gjenglemt er ikke en streng, det er en liste",vedstatus,koieID);
 				endreUtstyrStatus();
+				oppdaterVedStatus();
+				oppdaterGjenglemt();
 			}			
 		}catch (Exception e){
 			System.err.println(e.getStackTrace());
 			
 		}finally {
-			System.out.println(koieID);
-			System.out.println(vedstatus);
-			System.out.println(odelagtUtstyr);
-			System.out.println(gjenglemteTing);
+			PrintWriter writer = new PrintWriter("rapportTest.txt");
+			writer.print("koie1¤35¤odelagt1.1;odelagt1.2;odelagt1.3¤glemt1.1;glemt1.2"+"\r\n"
+						+"koie2¤27¤odelagt2.1;odelagt2.2;odelagt2.3¤glemt2.1;glemt2.2"+"\r\n"
+						+"koie3¤16¤odelagt3.1;odelagt3.2;odelagt3.3¤glemt3.1;glemt3.2"+"\r\n"
+						+"koie4¤57¤odelagt4.1;odelagt4.2;odelagt4.3¤glemt4.1;glemt4.2");
+			writer.close();
 		}
 	}
 	public void endreUtstyrStatus(){
@@ -78,15 +82,15 @@ public class Rapport {
 	
 	public void oppdaterGjenglemt(){
 		for(int j = 0; j<gjenglemteTing.size(); j++){
-			connection.leggInnGjenglemteUtstyr(koieID,gjenglemteTing.get(j));
+//			connection.leggInnGjenglemteUtstyr(koieID,gjenglemteTing.get(j));
 		}
 	}
 
 	public static void main(String[] args) throws IOException {
 		//kjører testfil
-//		Rapport test = new Rapport();
-//		String filename = "test.txt";
-//		FileReader file = new FileReader(filename);
-//		test.lesRapport(file);
+		Rapport test = new Rapport();
+		String filename = "rapportTest.txt";
+		FileReader file = new FileReader(filename);
+		test.lesRapport(file);
 	}
 }
