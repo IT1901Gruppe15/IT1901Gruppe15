6940da32cffe4fd266fa1caec15283704019bb62
morten2311 <morten23_lundenes@hotmail.com>
2014-09-25 11:57:02 +0200

    Reservasjon fra fil, mer eller mindre ferdig

--- src/klasser/Reservasjon.java
+++ src/klasser/Reservasjon.java
@@ -2,8 +2,6 @@ package klasser;
 
 import java.io.BufferedReader;
 
-
-
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileReader;
@@ -11,74 +9,146 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import textfiles.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Reservasjon {
 	String koieID;
-	private String epost,start_dato,slutt_dato;
+	private String epost, start_dato, slutt_dato;
 	DBConnection db = new DBConnection();
-	public void dateIsValid(){
-		
-	}
-	public void lesReservasjon(){
-		BufferedReader reader=null; 
-		
-		try{		
-		InputStream inputStream = Reservasjon.class.getResourceAsStream("testinput");
+	private Pattern pattern;
+	private Matcher matcher;
+	private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
-		reader=new BufferedReader(new InputStreamReader(inputStream));
-		String line = null;	
-		line = reader.readLine();
-		String[] list = line.split(";");
-		for (int i=0;i<list.length;i++){
-			switch(i){
-			case 0:
-				koieID = list[0];
-				break;
-			case 1:
-				epost=list[1];
-				break;
-			case 2:
-				start_dato=list[2];
-				break;
-			case 3:
-				slutt_dato=list[3];
-			
+	public Reservasjon() {
+		pattern = Pattern.compile(EMAIL_PATTERN);
+	}
+
+	// string til dato for validering av dato før, dato etter.
+	public Date asDate(String date, String dateFromat) throws ParseException {
+		SimpleDateFormat s = new SimpleDateFormat(dateFromat);
+		s.setLenient(false);
+		Date dat = s.parse(date);
+		return dat;
+	}
+
+	// valider gyldig dato
+	public boolean isThisDateValid(String dateToValidate, String dateFromat) {
+
+		if (dateToValidate == null) {
+			return false;
+		}
+
+		SimpleDateFormat sdf = new SimpleDateFormat(dateFromat);
+
+		sdf.setLenient(false);
+
+		try {
+
+			Date date = sdf.parse(dateToValidate);
+			System.out.println(date);
+
+		} catch (ParseException e) {
+
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
+	}
+
+	// valider epost
+	public void validate(final String hex) throws IllegalArgumentException {
+
+		matcher = pattern.matcher(hex);
+		System.out.println(hex);
+		if (!matcher.matches()) {
+			throw new IllegalArgumentException("feil epost format");
+		}
+
+	}
+
+	public void lesReservasjon() {
+		BufferedReader reader = null;
+
+		try {
+			InputStream inputStream = Reservasjon.class
+					.getResourceAsStream("testinput");
+
+			reader = new BufferedReader(new InputStreamReader(inputStream));
+			String line = null;
+			line = reader.readLine();
+			String[] list = line.split(";");
+			for (int i = 0; i < list.length; i++) {
+				switch (i) {
+				case 0:
+					koieID = list[0];// sjekke om koieid er gyldig?
+					break;
+				case 1:
+					validate(list[1].trim());
+					epost = list[1];
+
+					break;
+				case 2:
+					isThisDateValid(list[2], "yyyy-MM-dd");
+					start_dato = list[2];
+
+					break;
+				case 3:
+					isThisDateValid(list[3], "yyyy-MM-dd");
+					slutt_dato = list[3];
+					if (asDate(slutt_dato, "yyyy-MM-dd").compareTo(
+							asDate(start_dato, "yyyy-MM-dd")) <= 0) {
+						throw new IllegalArgumentException(
+								"Start dato kan ikke være etter slutt dato!");
+
+					}
+
+				}
 			}
-		}
-		
 
-		}catch (Exception e){
-		System.err.println(e.getStackTrace());
-			
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+
 		}
-			
-		
+
 	}
-	public void update(){
-		db.settinnReservasjon(getEpost(), getStart_dato(), getSlutt_dato(), getKoieID());
-		
+
+	public void updateDB() {
+		db.settinnReservasjon(getEpost(), getStart_dato(), getSlutt_dato(),
+				getKoieID());
+
 	}
 
 	public String getKoieID() {
 		return koieID;
 	}
+
 	public String getEpost() {
 		return epost;
 	}
+
 	public String getStart_dato() {
 		return start_dato;
 	}
+
 	public String getSlutt_dato() {
 		return slutt_dato;
 	}
+
 	public static void main(String[] args) {
 		Reservasjon res = new Reservasjon();
 		res.lesReservasjon();
-		res.update();
-		System.out.println(res.koieID + res.epost+res.start_dato + res.slutt_dato);
+
+		// res.update();
+		System.out.println(res.koieID + res.epost + res.start_dato
+				+ res.slutt_dato);
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
