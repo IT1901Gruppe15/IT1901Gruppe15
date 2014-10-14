979ef9dca0e121edba0c8f87fff648a2a4946beb
Joakim <joakigl@stud.ntnu.no>
2014-10-14 14:38:16 +0200

    Vedstatus ny dagtelling

--- src/core/Vedstatus.java
+++ src/core/Vedstatus.java
@@ -8,12 +8,11 @@ public class Vedstatus {
 	
 	ArrayList<Integer> tallX, tallY;
 	ArrayList<String> datoer;
-	double sumX=0, sumY=0, sumXY=0, sumXX=0, a, b, estimat;
-	int ar, maned, dag, ar2, maned2, dag2;
-	String nyDato;
+	double sumX=0, sumY=0, sumXY=0, sumXX=0, a, b;
+	int ar1, maned1, dag1, ar2, maned2, dag2, estimat, antallDager;
 	DBConnection dbconnect = new DBConnection();
 	
-	public void lagVedEstimat(int dagerBakover, String koieID, String dato) throws SQLException{
+	public int lagVedEstimat(String koieID, String dato) throws SQLException{
 		tallX = new ArrayList<Integer>();
 		tallY = new ArrayList<Integer>();
 		datoer = new ArrayList<String>();
@@ -25,45 +24,27 @@ public class Vedstatus {
 			s+=1;
 		}
 		tallX.add(0);
-//		ar = Integer.parseInt((datoer.get(1)).substring(0,4));
-//		maned = Integer.parseInt((datoer.get(1)).substring(5,7));
-//		dag = Integer.parseInt((datoer.get(1)).substring(8,10));
 		System.out.println(datoer);
 		for(int k = 1; k < tallY.size(); k++){
-			System.out.println(Integer.parseInt((datoer.get(k-1)).substring(8,10)));
-			if(Integer.parseInt((datoer.get(k)).substring(8,10)) - Integer.parseInt((datoer.get(k-1)).substring(8,10)) < 0){
-				maned2 = Integer.parseInt((datoer.get(k)).substring(5,7)) - 1;
-				if(maned2==0){
-					maned2 = 12;
-				}
-				int dagerManed = finnManed(maned2);
-				tallX.add(dagerManed-(Integer.parseInt((datoer.get(k-1)).substring(8,10)) - Integer.parseInt((datoer.get(k).substring(8,10)))));
-			} else {
-				tallX.add((Integer.parseInt((datoer.get(k)).substring(8,10)) - Integer.parseInt((datoer.get(k-1)).substring(8,10)))+(tallX.get(k-1)));
+			dag1 = Integer.parseInt((datoer.get(k-1)).substring(8,10));
+			maned1 = Integer.parseInt((datoer.get(k-1)).substring(5,7));
+			ar1 = Integer.parseInt((datoer.get(k-1)).substring(0,4));
+			dag2 = Integer.parseInt((datoer.get(k)).substring(8,10));
+			maned2 = Integer.parseInt((datoer.get(k)).substring(5,7));
+			ar2 = Integer.parseInt((datoer.get(k)).substring(0,4));
+			antallDager = ((ar2-ar1)*365);
+			for(int l = 1; l <= maned1; l++){
+				antallDager -= finnManed(l);
 			}
-//			ar2 = ar;
-//			if(dag-k <= 0){
-//				maned2 = maned - 1;
-//				if(maned2==0){
-//					ar2 = ar - 1;
-//					maned2 = 12;
-//				}
-//				int dagerManed = finnManed(maned2);
-//				dagerManed = dagerManed-(k-dag);
-//				dag2 = dagerManed;
-//			} else {
-//				dag2 = dag - k;
-//				maned2 = maned;
-//			}
-//			String nyDato = ar2+"-"+maned2+"-"+dag2;
+			for(int m = 1; m <= maned2; m++){
+				antallDager += finnManed(m);
+			}
+			antallDager += (finnManed(maned1)-dag1);
+			antallDager -= (finnManed(maned2)-dag2);
+			tallX.add(antallDager+tallX.get(k-1));
 		}
-			//String p = t.getString(1);
-			//tallY.add(Integer.parseInt(p));
-//		}
-//		finnDagerBakover();
 		
 		for(int i = 0; i < tallY.size(); i++){
-//			tallX.add(i);
 			sumXY += tallX.get(i)*tallY.get(i);
 			sumXX += tallX.get(i)*tallX.get(i);
 		}
@@ -75,7 +56,7 @@ public class Vedstatus {
 		
 		a = ((sumY*sumXX)-(sumX*sumXY))/((tallX.size()*sumXX)-(sumX*sumX));
 		b = ((tallX.size()*sumXY)-(sumX*sumY))/((tallX.size()*sumXX)-(sumX*sumX));
-		estimat = Math.floor((a*(-1))/b);
+		estimat = (int) Math.floor((a*(-1))/b);
 		
 		System.out.println("TallX: "+tallX);
 		System.out.println("TallY: "+tallY);
@@ -86,6 +67,7 @@ public class Vedstatus {
 		System.out.println("a: "+a);
 		System.out.println("b: "+b);
 		System.out.println("estimatet: "+estimat);
+		return estimat;
 	}
 
 	private int finnManed(int m) {
@@ -100,6 +82,6 @@ public class Vedstatus {
 
 	public static void main(String[] args) throws SQLException {
 		Vedstatus test = new Vedstatus();
-		test.lagVedEstimat(14,"Flaakoia","2014-10-01");
+		test.lagVedEstimat("Flaakoia","2014-10-01");
 	}
 }
