f5a01bbffca3c8715ec3806270a8355c7010fff9
mariuslundbo <marius.lundbo@gmail.com>
2014-10-14 14:58:04 +0200

    DBConnection fix

--- src/core/DBConnection.java
+++ src/core/DBConnection.java
@@ -284,7 +284,7 @@ public class DBConnection { // noen andre får oppdatere de siste javadocene i de
 		String q = ("delete from ErOdelagt where UtstyrID = (select UtstyrsID from Utstyr where Navn = '" + tingnavn + "' and FraktesTilID = '" + koie + "');");
 		db.oppdaterDB(q);
 		
-		q = ("update Utstyr set status '1' where Navn = '" + tingnavn + "' and FraktesTilID = '" + koie + "';");
+		q = ("update Utstyr set status = '1' where Navn = '" + tingnavn + "' and FraktesTilID = '" + koie + "';");
 		db.oppdaterDB(q);
 	}
 	
